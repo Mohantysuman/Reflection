5b2c65642d11bcc1751a7eece7d454b75b7fec20
Mohantysuman <105624690+Mohantysuman@users.noreply.github.com>
2022-06-10 17:09:15 +0530

    Add files via upload

---
+++ user.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+
+namespace Reflectionhandson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student obj = new Student();
+            Type type = typeof(Student);
+            var member = type.GetMembers();
+            var prop = type.GetProperties();
+            var method = type.GetMethods();
+        }
+    }
+}
